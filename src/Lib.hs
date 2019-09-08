9c8a2515e86faf781f0649f6a3c49e914cb059c4
Danilo Amoroso <dan.j.amoroso@gmail.com>
2019-09-08 15:47:46 +0200

    refactored parser to use Parsec combinators

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,48 @@
 module Lib
     ( someFunc
+    , Submodule
+    , parseSubmodule
     ) where
 
+import           Control.Applicative hiding (many)
+import           Data.List           (intercalate)
+import           Text.Format
+import           Text.Parsec         (Parsec, between, many, noneOf, parse,
+                                      spaces, string, (<?>))
+
+type Name = String
+type Path = String
+type Url = String
+
+data Submodule = Submodule Name Path Url deriving (Show, Eq)
+
+parseSubmodule :: Parsec String () Submodule
+parseSubmodule = do
+  name <- between subOpen subClose subName
+  spaces
+  path <- between pathOpen spaces text
+  url <- between urlOpen spaces text
+  return $ Submodule name path url
+  where
+    subOpen = string "[submodule \""
+    subClose = string "\"]"
+    subName = many $ noneOf "\""
+    pathOpen = string "path = "
+    urlOpen = string "url = "
+    text = many $ noneOf " \n\t"
+
+writeSubmodule :: Submodule -> String
+writeSubmodule (Submodule name path url) =
+  format "[submodule \"{0}\"]\n\tpath = {1}\n\turl = {2}\n" [name, path, url]
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  subModString <- readFile ".gitmodules"
+  let subModule = parse parseSubmodule ".gitmodules" subModString
+  putStrLn $ show subModule
+  let subModules = parse (many parseSubmodule) ".gitmodules" subModString
+  putStrLn $ show subModules
+  putStrLn subModString
+  case subModules of
+    Left _           -> putStrLn "error!"
+    Right submodules -> putStrLn $ concat $ fmap writeSubmodule submodules
